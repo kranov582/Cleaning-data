a288ed8e0f69023843d7710567283821c5b6347a
kranov582 <97236661+kranov582@users.noreply.github.com>
2022-05-10 17:55:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-covid_db = pd.read_csv('HIST_PAINEL_COVIDBR_2022_Parte1_09mai2022.csv')
+covid_db = pd.read_csv('HIST_PAINEL_COVIDBR_2022_Parte1_09mai2022.csv',sep = ';')
+pd.set_option('display.max_columns', None)
+print(covid_db.head())
+print(covid_db.shape)
+print(covid_db.describe())
+print(covid_db.isnull().sum())
+print(covid_db.loc[pd.isnull(covid_db['estado'])])
+covid_db = covid_db.dropna(subset=['estado', 'municipio'])
+covid_db = covid_db.drop(['Recuperadosnovos', 'emAcompanhamentoNovos'], axis=1)
 
-print(covid_db.head(147))
+print(covid_db.isnull().sum())
+print(covid_db.head())
+print(covid_db.describe())
